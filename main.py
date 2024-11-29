85a6106270c4510260e521b50d82b2721671515e
Tahir <tahirhsnli246@gmail.com>
2024-11-29 15:51:41 +0400

    asd

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
 from database import init_db
 from routers.UserRouter import router as user_router
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Railway PORT dəyişəni və ya 8000 default
+    app.run(app, host="0.0.0.0", port=port)
 
 app.add_middleware(
     CORSMiddleware,
